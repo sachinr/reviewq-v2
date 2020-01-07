2bc5469a59971cad43ed62a323fdf03adc7ddb71
Sachin Ranchod <sachin.ranchod@gmail.com>
2020-01-07 11:03:16 -0800

    add dotenv and fix indentation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
+import dotenv from "dotenv";
 import express from "express";
+// initialize configuration
+dotenv.config();
+
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.SERVER_PORT; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+  res.send( "Hello world!" );
 } );
 
 // start the Express server
 app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
+  // tslint:disable-next-line:no-console
+  console.log( `server started at http://localhost:${ port }` );
 } );

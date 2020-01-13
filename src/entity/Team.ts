2595f04c64f8a4d0d3c3ba715df01654757dd28d
Sachin Ranchod <sachin.ranchod@gmail.com>
2020-01-13 14:39:50 -0800

    add test for team.users

--- src/entity/Team.ts
+++ src/entity/Team.ts
@@ -1,4 +1,4 @@
-import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import {Column, Entity, JoinTable, OneToMany, PrimaryGeneratedColumn} from "typeorm";
 import {User} from "./User";
 
 @Entity()
--- src/test/team.test.ts
+++ src/test/team.test.ts
@@ -1,5 +1,60 @@
+import {createConnection, getConnection, getRepository} from "typeorm";
+import {Item} from "../entity/Item";
 import {Team} from "../entity/Team";
+import {User} from "../entity/User";
 
-test("basic", () => {
-  expect(1).toBe(1);
+beforeEach(() => {
+  return createConnection({
+    database: ":memory:",
+    dropSchema: true,
+    entities: [User, Team, Item],
+    logging: false,
+    synchronize: true,
+    type: "sqlite",
+  });
+});
+
+afterEach(() => {
+  const conn = getConnection();
+  return conn.close();
+});
+
+const setupTeam = async () => {
+  const team = new Team();
+  team.name = "LLL";
+  team.botSlackId = "B1234";
+  team.botToken = "xoxb-1234-1234";
+  await getRepository(Team).save(team);
+
+  return team;
+};
+
+const setupUser = async (team: Team) => {
+  const user = new User();
+  user.firstName = "Joe";
+  user.lastName = "Shmoe";
+  user.team = team;
+  user.slackId = "U1234";
+  user.slackUserName = "joeshmoe";
+
+  await getRepository(User).save(user);
+
+  return user;
+};
+
+test("store team and fetch it", async () => {
+  const team = await setupTeam();
+  const teams = await getRepository(Team).find();
+  expect(teams.length).toBe(1);
+  expect(teams[0].name).toBe("LLL");
+  expect(teams[0].botSlackId).toBe("B1234");
+  expect(teams[0].botToken).toBe("xoxb-1234-1234");
+});
+
+test("team should have users", async () => {
+  let team = await setupTeam();
+  const user = await setupUser(team);
+  team = await getRepository(Team).findOne(1, { relations: ["users"] });
+  expect(team.users.length).toBe(1);
+  expect(team.users[0].firstName).toBe("Joe");
 });

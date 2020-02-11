989293ee81d3aff524f9f05ab14b0797cd22d991
Sachin Ranchod <sachin.ranchod@gmail.com>
2020-02-11 13:02:47 -0800

    install url uses env var

--- src/controller/IndexController.ts
+++ src/controller/IndexController.ts
@@ -6,6 +6,6 @@ export class IndexController {
     const scopes = ["app_mentions:read", "channels:join", "channels:read", "chat:write", "commands",
       "groups:read", "im:history", "im:read", "mpim:read", "reactions:read", "reactions:write",
       "team:read", "users:read"];
-    return "https://slack.com/oauth/v2/authorize?client_id=22349320545.905600286708&scope=" + scopes.join();
+    return `https://slack.com/oauth/v2/authorize?client_id=${process.env.SLACK_CLIENT_ID}&scope=${scopes.join()}`;
   }
 }
